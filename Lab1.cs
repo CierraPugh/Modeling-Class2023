995a7c3ca605635c78afd68045a1e8b8cb0b9641
BonesAndFire <122629564+CierraPugh@users.noreply.github.com>
2023-09-05 16:18:38 -0700

    practicing stuff

---
+++ Lab1.cs
@@ -0,0 +1,17 @@
+Lab1 Practice
+
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "Cierra";
+Console.WriteLine(name);
+		int myNum = 17;
+Console.WriteLine(myNum);
+     }
+  }
+}
